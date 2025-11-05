c5b1bd748886fd9400da7c61e1723ec451aaf7f1
Aleeza Rasheed <aleeza@Aleezas-MacBook-Pro-2.local>
2025-11-05 15:58:34 -0600

    Final Spectral Hashing Example Complete

--- SpectrumWithTransformations.py
+++ SpectrumWithTransformations.py
@@ -13,18 +13,18 @@ class SpectrumWithTransformation(MsmsSpectrum):
         mz_array: Union[np.ndarray, Iterable],
         intensity_array: Union[np.ndarray, Iterable],
         retention_time: float = np.nan,
-        annotation_dictionary= None,
-        binned_mz: Union[np.ndarray, Iterable]=None,
-        hashed_mz: Union[np.ndarray, Iterable]=None
+        annotation_dictionary: dict | None = None,
+        binned_mz: np.ndarray[np.int_] | None = None,
+        hashed_mz: np.ndarray[np.float_] | None = None,
+        hashed_intensity: np.ndarray[np.float_] | None = None
     ) -> None:
         
         super().__init__(identifier, precursor_mz, precursor_charge, mz_array, intensity_array, retention_time)
-        # self.mz_array = np.asarray(mz_array, dtype=float)
-        # self.intensity_array = np.asarray(intensity_array, dtype=float)
         self.scan_number = int(scan_number)
-        self.annotation_dictionary = None if binned_mz is None else dict(annotation_dictionary)
-        self.binned_mz = None if binned_mz is None else np.asarray(binned_mz, dtype=float)
+        self.annotation_dictionary = None if annotation_dictionary is None else dict(annotation_dictionary)
+        self.binned_mz = None if binned_mz is None else np.asarray(binned_mz, dtype=int)
         self.hashed_mz = None if hashed_mz is None else np.asarray(hashed_mz, dtype=float)
+        self.hashed_intensity = None if hashed_intensity is None else np.asarray(hashed_intensity, dtype=float)
 
     def __getstate__(self):
         return {
@@ -33,7 +33,8 @@ class SpectrumWithTransformation(MsmsSpectrum):
             "scan_number": self.scan_number,
             "annotation_dictionary": self.annotation_dictionary,
             "binned_mz": self.binned_mz,
-            "hashed_mz": self.hashed_mz
+            "hashed_mz": self.hashed_mz,
+            "hashed_intensity": self.hashed_intensity
         } 
     
     def __setstate__(self, state):
@@ -46,6 +47,9 @@ class SpectrumWithTransformation(MsmsSpectrum):
         )
         self.hashed_mz = (
             None if state.get("hashed_mz") is None else np.asarray(state["hashed_mz"], dtype=float)
+        )
+        self.hashed_intensity = (
+            None if state.get("hashed_intensity") is None else np.asarray(state["hashed_intensity"], dtype=float)
         )
     
 
